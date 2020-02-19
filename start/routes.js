d83ba7520f005b68e3222af3f31f9ee76ca2dfb9
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-02-19 19:58:31 -0300

    :rocket: Adiciona validações

--- start/routes.js
+++ start/routes.js
@@ -1,30 +1,21 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('sessions', 'SessionController.store')
-Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
+Route.post('users', 'UserController.store').validator('User')
 
 Route.group(() => {
-  Route.resource('teams', 'TeamController').apiOnly()
+  Route.resource('teams', 'TeamController')
+    .apiOnly()
+    .validator(new Map([[['teams.store', 'teams.update'], ['Team']]]))
 }).middleware('auth')
 
 Route.group(() => {
-  Route.post('invites', 'InviteController.store')
+  Route.post('invites', 'InviteController.store').validator('Invite')
 
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store', 'projects.update'], ['Project']]]))
 }).middleware(['auth', 'team'])
