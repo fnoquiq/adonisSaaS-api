97852f0017670016dbd5df18ea7192a6d6165f6c
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-02-21 06:02:10 -0300

    :rocket: Configura Acl

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,10 @@ class User extends Model {
     })
   }
 
+  teamJoins() {
+    return this.hasMany('App/Models/UserTeam')
+  }
+
   tokens() {
     return this.hasMany('App/Models/Token')
   }
@@ -29,6 +33,30 @@ class User extends Model {
     return this.belongsToMany('App/Models/Team').pivotModel(
       'App/Models/UserTeam'
     )
+  }
+
+  async can(expression) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+
+    return team.can(expression)
+  }
+
+  async is(expression) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+
+    return team.is(expression)
+  }
+
+  async scope(required) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+
+    return team.scope(required)
   }
 }
 
